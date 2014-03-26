3baa3f46a017eededdbd5eede9630b498705018f
Edward Ekstrom <edward.ekstrom@gmail.com>
2014-03-25 20:31:09 -0600

    added kmeans

---
+++ src/clustering/Kmeans.java
@@ -0,0 +1,20 @@
+package clustering;
+
+import toolKit.Matrix;
+import toolKit.SupervisedLearner;
+
+public class Kmeans extends SupervisedLearner {
+
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
