e28bbeb16bcfbb934682f247cfff83a11ea5e9e9
Edward Ekstrom <edward.ekstrom@gmail.com>
2014-04-09 18:49:49 -0600

    fixed instanceOutputs and _traingingInput having different lenghts

--- src/toolKit/MLSystemManager.java
+++ src/toolKit/MLSystemManager.java
@@ -21,7 +21,10 @@ public class MLSystemManager {
 	public SupervisedLearner getLearner(String model, Random rand) throws Exception
 	{
 //		int[] structure = {4, 8, 3};
-		int[] structure = {10, 20, 10};
+//		int[] structure = {10, 20, 10};
+//		int[] structure = {12, 24, 27};
+//		int[] structure = {12, 24, 24};
+		int[] structure = {3, 12, 40};
 		
 		if (model.equals("baseline")) return new BaselineLearner();
 		else if (model.equals("perceptron")) return new Perceptron(rand);
@@ -33,12 +36,12 @@ public class MLSystemManager {
 
 	public void run(String[] args) throws Exception {
 //		./MLSystemManager -L [LearningAlgorithm] -A [ARFF_File] -E random [PercentageForTraining]
-//		args = new String[]{"-L", "baseline", "-A", "data/vowel.arff", "-E", "cross", "10", "-N"};
+		args = new String[]{"-L", "baseline", "-A", "data/streaming_e_s_ascii.csv", "-E", "cross", "10", "-N"};
 //		args = new String[]{"-L", "neuralnet", "-A", "data/iris.arff", "-E", "cross", "10", "-N"};
-//		args = new String[]{"-L", "neuralnet", "-A", "data/vowel.arff", "-E", "training"};
+//		args = new String[]{"-L", "neuralnet", "-A", "data/streaming_e_s_ascii.csv", "-E", "cross", "10", "-N"};
 //		args = new String[]{"-L", "neuralnet", "-A", "data/test.arff", "-E", "training"};
 //		args = new String[]{"-L", "neuralnet", "-A", "data/iris.arff", "-E", "random", ".75", "-N"};
-		args = new String[]{"-L", "neuralnet", "-A", "data/vowel.arff", "-E", "random", ".75", "-N"};
+//		args = new String[]{"-L", "neuralnet", "-A", "data/voting.arff", "-E", "random", ".75", "-N"};
 //		args = new String[]{"-L", "perceptron", "-A", "data/iris.arff", "-E", "cross", "10", "-N"};
 //		args = new String[]{"-L", "perceptron", "-A", "data/iris.arff", "-E", "training", "-N"};
 //		args = new String[]{"-L", "perceptron", "-A", "data/linearlySeparable.arff", "-E", "training", "-N"};
@@ -183,7 +186,8 @@ public class MLSystemManager {
 					elapsedTime += System.currentTimeMillis() - startTime;
 					double accuracy = learner.measureAccuracy(testFeatures, testLabels, null);
 					sumAccuracy += accuracy;
-					System.out.println("Rep=" + j + ", Fold=" + i + ", Accuracy=" + accuracy);
+//					System.out.println("Rep=" + j + ", Fold=" + i + ", Accuracy=" + accuracy);
+					System.out.println(accuracy);
 				}
 			}
 			elapsedTime /= (reps * folds);
